3b346098df49c92c6fe13f11e4780b7f50d3aa4b
Leandro Marques da Cunha <leandromcunha@gmail.com>
2017-01-24 21:46:44 -0200

    Estudo com criar tabela com rails

---
+++ db/migrate/20170124234223_create_professionals.rb
@@ -0,0 +1,12 @@
+class CreateProfessionals < ActiveRecord::Migration[5.0]
+  def change
+    create_table :professionals do |t|
+      t.string, :name
+      t.datetime, :birthdate
+      t.string, :gender
+      t.string, :country
+      t.string :type
+      t.timestamps
+    end
+  end
+end
