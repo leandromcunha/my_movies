e94ea8e64994800bba87f5177cf14ce99ecee76f
Leandro Marques da Cunha <leandromcunha@gmail.com>
2017-01-26 23:38:55 -0200

    Finalizando a aula 3 com css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,11 +4,63 @@ class MoviesController < ApplicationController
   end
 
   def show
+    id = params['id']
+    begin
+      @movies = Movie.find id
+    rescue ActiveRecord::RecordNotFound
+      render file: "#{Rails.root}/public/404.html" , status: 404
+    end
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new( movie_params )
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :new
+    end
   end
+
+  def edit
+    id = params['id']
+    begin
+      @movie = Movie.find id
+    rescue ActiveRecord::RecordNotFound
+      render file: "#{Rails.root}/public/404.html" , status: 404
+    end
+  end
+
+  def update
+    id = params['id']
+    begin
+      @movie = Movie.find id
+      @movie.update(movie_params)
+      if @movie.save
+        redirect_to action: :show, id: @movie.id
+      else
+        render :new
+      end
+    rescue ActiveRecord::RecordNotFound
+      render file: "#{Rails.root}/public/404.html" , status: 404
+    end
+  end
+
+  def remove
+    id = params['id']
+    begin
+      @movies = Movie.delete id
+    rescue ActiveRecord::RecordNotFound
+      render file: "#{Rails.root}/public/404.html" , status: 404
+    end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title,:release_date,:description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'movies/index'
+  get 'movies'=> 'movies#index'
+  get 'movies/new' => 'movies#new'
+  get 'movies/:id' => 'movies#show'
+  post 'movies'    => 'movies#create'
+  get 'movies/:id/edit' => 'movies#edit'
+  put 'movies/:id' => 'movies#update'
 
-  get 'movies/show'
 
-  get 'movies/new'
 
-  get 'movies/create'
+  #get 'movies/show'
+
+  #get 'movies/new'
+
+  #get 'movies/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'movie' => 'movies#index'
+  #get 'movie' => 'movies#index'
 end
